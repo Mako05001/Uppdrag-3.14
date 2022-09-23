40acf8113ad0175fcc80e963603ae560dd4c426e
MAKSY <MAKSY@DESKTOP-NT4SAN5>
2022-09-23 18:22:31 +0200

    Add project files.

---
+++ Uppdrag 3.14/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Uppgift3._14
+{
+    class program
+    {
+       static void Main(string[]args)
+        {
+            Console.WriteLine("Har du gått på gymnasiet? j/n");
+            string L = Console.ReadLine();
+            Console.WriteLine("Hur gammal är du?");
+            string Å = Console.ReadLine();
+            int Ålder = Convert.ToInt32(Å);
+            if (L == "j" && Ålder <= 22 && Ålder >= 19)
+            {
+                Console.WriteLine("Vi vill gärna anställa dig");
+            }
+            else
+            {
+                Console.WriteLine("Vi letar tyvär efter annan personal just nu");
+            }
+        }
+    }
+}
